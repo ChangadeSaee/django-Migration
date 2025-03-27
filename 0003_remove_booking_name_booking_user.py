e453920c199554c5deec180bca52ada764152da3
ChangadeSaee <saeechangade92@gmail.com>
2025-03-27 22:41:16 +0530

    Update 0003_remove_booking_name_booking_user.py

--- 0003_remove_booking_name_booking_user.py
+++ 0003_remove_booking_name_booking_user.py
@@ -5,6 +5,7 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
